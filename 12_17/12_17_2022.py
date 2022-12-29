977f47db95350b203794df9502644af036746991
Jonathan Dai <yfjd01@gmail.com>
2022-12-29 17:58:18 -0500

    12_17 part 1 able to drop

--- 12_17/12_17_2022.py
+++ 12_17/12_17_2022.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from aoc_util import shape1, shape2, shape3, shape4, shape5, convertLinesIntoSparceMatList
+from aoc_util import shape1, shape2, shape3, shape4, shape5, convertLinesIntoSparceMatList, printRockInChamber, findMaxHeightOfRocksInChamber
 
 class Solution():
     
@@ -12,41 +12,67 @@ class Solution():
     def main(self):
         # self.figureOutPositionBeforeDrop(shape2)
         curRock = convertLinesIntoSparceMatList(shape2)
-        self.rockLandsInChamber(curRock)
-        self.printRockInChamber()
+        self.dropRock(curRock)
+        
+        printRockInChamber(self.rocksInChamber)
         return None
 
-    # Each rock appears so that its left edge is two units away from the left wall and its bottom edge is three units above the highest rock in the room (or the floor, if there isn't one).
-    def figureOutPositionBeforeDrop(self, shape):
-        print(shape[0][1])
+    def dropRock(self, curRock):
+        startDisFromLeftWall = 2 # Each rock appears so that its left edge is two units away from the left wall and 
+        maxHeight = findMaxHeightOfRocksInChamber(self.rocksInChamber)
+        startHeight = 3 + maxHeight # its bottom edge is three units above the highest rock in the room (or the floor, if there isn't one).
+
+        print(startHeight)
+
+        newRock = defaultdict(lambda: defaultdict(str))
+        for y in curRock:
+            for x in curRock[y]:
+                newRock[y + startHeight][x + startDisFromLeftWall] = curRock[y][x]
+        curRock = newRock
+
+        while True:
+            noCollisionIfMove = True
+            # Will there be collision if we move stuff down one more step?
+            for y in curRock:
+                for x in curRock[y]:
+                    if (self.isThereCollisionForCoord(y-1, x)):
+                        noCollisionIfMove = False
+                if (noCollisionIfMove == False):
+                    break
+
+            
+            # if not move it
+            if (noCollisionIfMove == True):
+                tempRock = defaultdict(lambda: defaultdict(str))
+                for y in curRock:
+                    for x in curRock[y]:
+                        tempRock[y - 1][x] = curRock[y][x]
+                curRock = tempRock
+            else:
+                break
+
+        # Set in stone (write to rockInChamber)
+        self.rockLandsInChamber(curRock)
         return None
-    
+
+
+    def isThereCollisionForCoord(self, y, x):
+        if (y < 0):
+            return True
+        
+        if (x >= 7 or x < 0):
+            return True
+
+        if (self.rocksInChamber[y][x] == True):
+            return True
+
+        return False
+ 
     def rockLandsInChamber(self, rock):
         for y in rock:
             for x in rock[y]:
                self.rocksInChamber[y][x] = True
         return None
-
-
-    def printRockInChamber(self):
-        maxHeight = float('-inf')
-        for y in self.rocksInChamber:
-            maxHeight = max(maxHeight, y)
-
-        print('+-------+')
-        for y in range(maxHeight, -1, -1):
-            line = ['.'] * 7
-
-            for x in self.rocksInChamber[y]:
-                line[x] = '#'
-            
-            printLine = ''
-            for i in line:
-                printLine += i
-            
-            print('|' + printLine + '|:' + str(y))
-        print('+-------+')
-
 
 
 sol = Solution()
--- 12_17/aoc_util.py
+++ 12_17/aoc_util.py
@@ -38,3 +38,26 @@ def convertLinesIntoSparceMatList(shape):
             if (charVal == '#'):
                 curRock[lineIndex][charIndex] = True
     return curRock
+
+def findMaxHeightOfRocksInChamber(rocksInChamber):
+    maxHeight = 0
+    for y in rocksInChamber:
+        maxHeight = max(maxHeight, y)
+    return maxHeight
+
+def printRockInChamber(rocksInChamber):
+    maxHeight = findMaxHeightOfRocksInChamber(rocksInChamber)
+    print('+-------+')
+    for y in range(maxHeight, -1, -1):
+        line = ['.'] * 7
+
+        for x in rocksInChamber[y]:
+            if (rocksInChamber[y][x] == True):
+                line[x] = '#'
+        
+        printLine = ''
+        for i in line:
+            printLine += i
+        
+        print('|' + printLine + '|:' + str(y))
+    print('+-------+')
